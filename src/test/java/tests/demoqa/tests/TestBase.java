7d201cf319a01371485efc4190e56e001343ad31
Irina Buchneva <buchneva.irina986@gmail.com>
2022-10-16 17:44:27 +0300

    added attachments

--- src/test/java/tests/demoqa/tests/TestBase.java
+++ src/test/java/tests/demoqa/tests/TestBase.java
@@ -2,7 +2,9 @@ package tests.demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
@@ -14,5 +16,12 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
+
+    @AfterEach
+    static void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
     }
 }
