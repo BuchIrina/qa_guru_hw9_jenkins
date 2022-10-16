92bdeb07183cc5d1d1cc709179c3ff20096394e0
Irina Buchneva <buchneva.irina986@gmail.com>
2022-10-16 17:13:54 +0300

    added fill form tests

---
+++ src/test/java/tests/com/buchneva/pages/RegistrationFormPage.java
@@ -0,0 +1,132 @@
+package tests.com.buchneva.pages;
+
+import com.codeborne.selenide.SelenideElement;
+import tests.com.buchneva.pages.components.CalendarComponent;
+import tests.com.buchneva.pages.components.ResultsModal;
+import tests.com.buchneva.pages.components.StateAndCityComponent;
+
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+public class RegistrationFormPage {
+
+    private final static String TITLE_TEXT = "Student Registration Form";
+    private CalendarComponent calendarComponent = new CalendarComponent();
+    private ResultsModal resultsModal = new ResultsModal();
+    private StateAndCityComponent stateAndCityComponent = new StateAndCityComponent();
+    private SelenideElement
+            firstNameInput = $("#firstName"),
+            lastNameInput = $("#lastName"),
+            emailInput = $("#userEmail"),
+            genderInput = $("#genterWrapper"),
+            numberInput = $("#userNumber"),
+            dateOfBirthInput = $("#dateOfBirthInput"),
+            subjectsInput = $("#subjectsInput"),
+            hobbiesInput = $("#hobbiesWrapper"),
+            pictureInput = $("#uploadPicture"),
+            addressInput = $("#currentAddress"),
+            submit = $("#submit");
+
+    public RegistrationFormPage openPage() {
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text(TITLE_TEXT));
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
+
+        return this;
+    }
+
+    public RegistrationFormPage setFirstName(String value) {
+        firstNameInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationFormPage clearFirstName() {
+        firstNameInput.clear();
+
+        return this;
+    }
+
+    public RegistrationFormPage setLastName(String value) {
+        lastNameInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationFormPage setEmail(String value) {
+        emailInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationFormPage setGender(String value) {
+        genderInput.$(byText(value)).click();
+
+        return this;
+    }
+
+    public RegistrationFormPage setNumber(String value) {
+        numberInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationFormPage setBirthDate(String day, String month, String year) {
+        dateOfBirthInput.click();
+        calendarComponent.setDate(day, month, year);
+
+        return this;
+    }
+
+    public RegistrationFormPage setSubjects(String value) {
+        subjectsInput.sendKeys(value);
+        subjectsInput.pressEnter();
+
+        return this;
+    }
+
+    public RegistrationFormPage setHobbies(String value) {
+        hobbiesInput.$(byText(value)).click();
+
+        return this;
+    }
+
+    public RegistrationFormPage uploadPicture(String value) {
+        pictureInput.uploadFromClasspath(value);
+
+        return this;
+    }
+
+    public RegistrationFormPage setAddress(String value) {
+        addressInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationFormPage setStateAndCity(String state, String city) {
+        stateAndCityComponent.setStateAndCity(state, city);
+
+        return this;
+    }
+
+    public RegistrationFormPage clickSubmit() {
+        submit.click();
+
+        return this;
+    }
+
+
+    public RegistrationFormPage checkResultsTableVisible() {
+        resultsModal.checkVisible();
+
+        return this;
+    }
+
+    public RegistrationFormPage checkResult(String key, String value) {
+        resultsModal.checkResult(key, value);
+
+        return this;
+    }
+}
---
+++ src/test/java/tests/com/buchneva/tests/RegistrationFormWithPageObjectsTests.java
@@ -0,0 +1,55 @@
+package tests.com.buchneva.tests;
+
+import org.junit.jupiter.api.Test;
+import tests.com.buchneva.pages.RegistrationFormPage;
+
+import static io.qameta.allure.Allure.step;
+
+public class RegistrationFormWithPageObjectsTests extends TestBase {
+    RegistrationFormPage registrationFormPage = new RegistrationFormPage();
+    String firstName = "Darth";
+    String lastName = "Vader";
+    String email = "darty@gmail.com";
+    String mobile = "0123456789";
+    String subjects = "Eng";
+    String currentAddress = "Empire, Death Star";
+
+
+    @Test
+    void fillFormTest() {
+        step("Open registration form", () -> {
+            registrationFormPage.openPage();
+        });
+
+        step("Fill form", () -> {
+            registrationFormPage.setFirstName(firstName)
+                    .setLastName(lastName)
+                    .setEmail(email)
+                    .setGender("Male")
+                    .setNumber(mobile)
+                    .setBirthDate("30", "November", "1986")
+                    .setSubjects(subjects)
+                    .setHobbies("Sports")
+                    .setHobbies("Reading")
+                    .setHobbies("Music")
+                    .uploadPicture("img/QA-Tester-meme-03.jpg")
+                    .setAddress(currentAddress)
+                    .setStateAndCity("NCR", "Noida")
+                    .clickSubmit();
+        });
+
+        step("Check form results", () -> {
+            registrationFormPage.checkResultsTableVisible()
+                    .checkResult("Student Name", firstName + " " + lastName)
+                    .checkResult("Student Email", email)
+                    .checkResult("Gender", "Male")
+                    .checkResult("Mobile", mobile)
+                    .checkResult("Date of Birth", "30 November,1986")
+                    .checkResult("Subjects", "English")
+                    .checkResult("Hobbies", "Sports, Reading, Music")
+                    .checkResult("Picture", "QA-Tester-meme-03.jpg")
+                    .checkResult("Address", currentAddress)
+                    .checkResult("State and City", "NCR Noida");
+        });
+    }
+}
